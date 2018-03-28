21a3b1d2b1e474ea2506db5dbf74670af1be5621
cycole1234 <cycole1234@gmail.com>
2018-03-29 01:42:55 +0800

    part 1 complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
-
+  get '/movies?id=title_header' => 'movies#index'
+  get '/movies?id=release_date_header' => 'movies#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
